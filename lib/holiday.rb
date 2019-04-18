cd8545716099f4262e8aa41f2ef5a2941078a8bb
Alex Borst <butler.borst@gmail.com>
2019-04-18 23:39:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.map do |holiday, supply|
       holiday if supply.include?("BBQ")
     end
-    holiday_hash.flatten.compact
+    holiday
   end
 end
 
